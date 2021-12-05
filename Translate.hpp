887286c76582ea66f3a1c6db3bb585077820badc
jojjiw <11812420@mail.sustech.edu.cn>
2021-12-05 20:40:39 +0800

    translate

--- Translate.hpp
+++ Translate.hpp
@@ -1,8 +1,14 @@
 #ifndef CS323_PROJ3_TRANSLATE_HPP
 #define CS323_PROJ3_TRANSLATE_HPP
 
+#include "spl_node.hpp"
+#include "InterCode.hpp"
+#include <vector>
+using namespace std;
 
 class Translate {
+public:
+    vector<InterCode> translate_cond_Exp(Node *exp, string l1, string l2);
 
 };
 
