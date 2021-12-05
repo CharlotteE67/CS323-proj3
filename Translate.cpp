b06d181d02a4a1f6f3d01d8027695390037e47a2
CharlotteE67 <11812419@mail.sustech.edu.cn>
2021-12-05 22:01:54 +0800

    finish translate_stmt

--- Translate.cpp
+++ Translate.cpp
@@ -1,4 +1,5 @@
 #include "Translate.hpp"
+#include <string>
 
 int cnt_place = 0, cnt_label = 0, cnt_var = 0;
 
@@ -10,6 +11,84 @@ string new_place() {
 string new_label() {
     ++cnt_label;
     return "l" + to_string(cnt_label);
+}
+
+
+vector<InterCode> translate_Stmt(Node *stmt) {
+    if (stmt->get_name() != "stmt")
+        return;
+    vector <InterCode> translate;
+    // RETURN Exp SEMI
+    if (stmt->child.size() == 3) {
+        string tp = new_place();
+        vector<InterCode> codes = translate_Exp(stmt->child[1], tp);
+        InterCode return_code = new InterCode();
+        translate.insert(translate.end(), codes.begin(), codes.end());
+        translate.push_back(new InterCode(18, tp)); // RETURN
+    }
+    // IF LP Exp RP Stmt
+    else if (stmt->child.size() == 5 && stmt->child[0].get_name() == "IF"){
+        string lb1 = new_label();
+        string lb2 = new_label();
+        vector<InterCode> code1 = translate_cond_Exp(stmt->child[2], lb1, lb2);
+        vector<InterCode> code2 = translate_Stmt(stmt->child[4]);
+        translate.insert(translate.end(), code1.begin(), code1.end());
+        translate.push_back(new InterCode(1, lb1));
+        translate.insert(translate.end(), code2.begin(), code2.end());
+        translate.push_back(new InterCode(1, lb2));
+    }
+    // IF LP Exp RP Stmt_1 ELSE Stmt_2
+    else if (stmt->child.size() == 7){
+        string lb1 = new_label();
+        string lb2 = new_label();
+        string lb3 = new_label();
+        vector<InterCode> code1 = translate_cond_Exp(stmt->child[2], lb1, lb2);
+        vector<InterCode> code2 = translate_Stmt(stmt->child[4]);
+        vector<InterCode> code3 = translate_Stmt(stmt->child[6]);
+        translate.insert(translate.end(), code1.begin(), code1.end());
+        translate.push_back(new InterCode(1, lb1));
+        translate.insert(translate.end(), code2.begin(), code2.end());
+        translate.push_back(new InterCode(11, lb3));
+        translate.push_back(new InterCode(1, lb2));
+        translate.insert(translate.end(), code3.begin(), code3.end());
+        translate.push_back(new InterCode(1, lb3));
+    }
+    // WHILE LP Exp RP Stmt
+    else if (stmt->child.size() == 5){
+        string lb1 = new_label();
+        string lb2 = new_label();
+        string lb3 = new_label();
+        vector<InterCode> code1 = translate_cond_Exp(stmt->child[2], lb2, lb3);
+        vector<InterCode> code2 = translate_Stmt(stmt->child[4]);
+        translate.push_back(new InterCode(1, lb1));
+        translate.insert(translate.end(), code1.begin(), code1.end());
+        translate.push_back(new InterCode(1, lb2));
+        translate.insert(translate.end(), code2.begin(), code2.end());
+        translate.push_back(new InterCode(11, lb1));
+        translate.push_back(new InterCode(1, lb3));
+    }
+    // Exp SEMI
+    else if (stmt->child.size() == 2){
+        string tp = new_place();
+        vector<InterCode> codes = translate_Exp(stmt->child[0], tp);
+        translate.insert(translate.end(), codes.begin(), codes.end());
+    }
+    // CompSt
+    else if (stmt->child.size() == 1){ }
+    // FOR LP Def Exp SEMI Exp RP Stmt
+    else if (stmt->child.size() == 8) { }
+    // FOR LP Exp SEMI Exp SEMI Exp RP Stmt
+    else if (stmt->child.size() == 9){
+        string tp1 = new_place();
+        string tp2 = new_place();
+        string lb1 = new_label();
+        string lb2 = new_label();
+        vector<InterCode> code1 = translate_Exp(stmt->child[2], tp1);
+        vector<InterCode> code2 = translate_cond_Exp(stmt->child[4], lb1, lb2);
+        vector<InterCode> code3 = translate_Exp(stmt->child[5], tp2);
+    }
+
+    return translate;
 }
 
 
--- Translate.hpp
+++ Translate.hpp
@@ -13,4 +13,7 @@ string new_label();
 vector<InterCode> translate_cond_Exp(Node *exp, string lt, string lf);
 
 vector<InterCode> translate_Exp(Node *exp,string t);
+
+vector<InterCode> translate_Stmt(Node *stmt);
+
 #endif //CS323_PROJ3_TRANSLATE_HPP
